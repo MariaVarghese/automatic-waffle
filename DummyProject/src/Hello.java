530914578edfca52180431f9375cf0cbd8e03bd8
Maria Varghese <mariavarghese@insidesherpa.com>
2021-03-23 17:41:20 +0530

    commit

--- DummyProject/src/Hello.java
+++ DummyProject/src/Hello.java
@@ -2,10 +2,10 @@
 public class Hello {
 	
 	public void method1() {
-		System.out.println("Line1");
-		System.out.println("Line2");
-		System.out.println("Line4");
-		System.out.println("Line3");
+		System.out.println("Line No. 1");
+		System.out.println("Line No. 2");
+		System.out.println("Line No. 3");
+		System.out.println("Line No. 4");
 	}
 	
 }
